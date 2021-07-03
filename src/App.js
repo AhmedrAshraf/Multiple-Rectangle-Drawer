35538f6d05e32eed890d46ba3f89d32a25b1f2df
ahmedrashraf <ahmedr.0331@gmail.com>
2021-07-03 19:57:55 +0500

    make more beauti

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import "./App.css";
+import Konva from "konva";
 import React, { useState } from "react";
-import { Stage, Layer, Rect } from "react-konva";
+import { Stage, Layer, Rect, Group } from "react-konva";
 
 function App() {
   const [x, setx] = useState(0);
   const [y, sety] = useState(0);
+  const [col, setCol] = useState("");
   const [rectangle, setRectangle] = useState([]);
   const [firstClick, setFirstClick] = useState(false);
 
-  function handleClick(event) {
-    var { clientX, clientY } = event;
+  function handleClick({ clientX, clientY }) {
+    let array = [...rectangle];
+    let color = Konva.Util.getRandomColor();
+    if (firstClick) {
+      let w = clientX - x;
+      let h = clientY - y;
+      array.push({ x, y, width: w, height: h, color: col });
+    } else {
+      setx(clientX);
+      sety(clientY);
+      setCol(color);
+    }
+    setFirstClick(!firstClick);
+    setRectangle(array);
+  }
+
+  function handleMove({ clientX, clientY }) {
     let array = [...rectangle];
     if (firstClick) {
       let w = clientX - x;
       let h = clientY - y;
-      array.push({ height: h, width: w, x, y });
-      setFirstClick(false);
-    } else {
-      setx(clientX);
-      sety(clientY);
-      setFirstClick(true);
+      let idx = array.length ? array.length - 1 : 0;
+      array[idx] = { color: col, height: h, width: w, x, y };
     }
     setRectangle(array);
   }
@@ -35,18 +48,28 @@ function App() {
           {!!rectangle?.length &&
             rectangle.map((e) => {
               return (
-                <Rect
-                  x={e.x}
-                  y={e.y}
-                  fill="gray"
-                  width={e.width}
-                  height={e.height}
-                />
+                <Group x={e.x} y={e.y}>
+                  <Rect
+                    noise={1}
+                    opacity={0.5}
+                    stroke="black"
+                    shadowBlur={1}
+                    fill={e.color}
+                    width={e.width}
+                    strokeWidth={1}
+                    height={e.height}
+                    filters={[Konva.Filters.Noise]}
+                  />
+                </Group>
               );
             })}
         </Layer>
       </Stage>
-      <div className="box box-a" onClick={handleClick}></div>
+      <div
+        className="box box-a"
+        onClick={handleClick}
+        onMouseMove={handleMove}
+      ></div>
     </div>
   );
 }
